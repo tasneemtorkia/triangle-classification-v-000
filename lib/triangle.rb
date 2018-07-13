63687b2edea53b944913dfad95b9ecc803b8a0a9
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:11:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if (@l1 == @l2) && (@l2 == @l3) :equilateral
     elsif (@l1 == @l2) || (@l2 == @l33) || (@l1 == @l3)
         :isosceles
-      elsif (@l1 != @l2) && (@l2 != @l3) && (@l1 != @l3)
+      else (@l1 != @l2) && (@l2 != @l3) && (@l1 != @l3)
         :scalene
   end 
 end 
