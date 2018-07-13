d95a99ce43f31a2be8116f23eea80ce59213618a
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:21:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,15 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
   end
 
-  def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+  def kind
+    if (@l1 <= 0) || (@l2 <= 0) || (@l3 <= 0)
       raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+    elsif (@l1 + @l2 <= @l3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
