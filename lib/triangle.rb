f3fcf61d702a605d2303f9d3a69c8001b435f63d
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:27:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,16 @@ class Triangle
   def kind
     if (@l1 <= 0) || (@l2 <= 0) || (@l3 <= 0)
       raise TriangleError
-    elsif (@l1 + @l2 <= @l3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      
+    elsif (@l1 + @l2 <= @l3) || (@l1 + @l3 <= @l2) || (@l2 + @l3 <= @l1)
+    
       raise TriangleError
+      
     else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+      if (@l1 == @l2) && (@l2 == @l3)
         :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        
+      elsif (@l1 == @l2) || (@l2 == @l3) || (@side_1 == @side_3)
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
