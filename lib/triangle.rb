11ad5cb3ec240ed2cfb17780d26255a1242d0a11
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:19:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,23 @@
 class Triangle
-  
-  attr_accessor :l1, :l2, :l3
-  
-  def initialize(l1, l2, l3)
-    @l1 = l1 
-    @l2 = l2 
-    @l3 = l3
-  end 
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
-  def kind
-    if (@l1 == @l2) && (@l2 == @l3) :equilateral
-    elsif (@l1 == @l2) || (@l2 == @l33) || (@l1 == @l3)
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
-      else (@l1 != @l2) && (@l2 != @l3) && (@l1 != @l3)
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
-  end 
-end 
+      end
+    end
 
-end
+  end
