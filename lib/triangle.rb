74d9546552a083002f2c3f99f924de5d33e5a9f3
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:11:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :l1, :l2, :l3
+  
+  def initialize(l1, l2, l3)
+    @l1 = l1 
+    @l2 = l2 
+    @l3 = l3
+  end 
+
+  def kind
+    if (@l1 == @l2) && (@l2 == @l3) :equilateral
+    elsif (@l1 == @l2) || (@l2 == @l33) || (@l1 == @l3)
+        :isosceles
+      elsif (@l1 != @l2) && (@l2 != @l3) && (@l1 != @l3)
+        :scalene
+  end 
+end 
+
 end
