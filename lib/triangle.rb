da31b1caf9a37cdaaf76b22d1295ac00e727e5a5
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-07-13 16:40:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
 
   def kind
     if (@l1 <= 0) || (@l2 <= 0) || (@l3 <= 0)
-      raise TriangleError
+      raise RangeError
       
     elsif (@l1 + @l2 <= @l3) || (@l1 + @l3 <= @l2) || (@l2 + @l3 <= @l1)
     
